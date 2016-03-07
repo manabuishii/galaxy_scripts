10599af71b77e80593fe49044067bcf25d464bc7
Manabu ISHII <manabu.ishii.rb@gmail.com>
2016-03-07 18:49:53 +0900

    Support get_histories specified id

--- galaxy_histories.py
+++ galaxy_histories.py
@@ -3,14 +3,20 @@ import json
 import sys
 from bioblend import galaxy
 
-if len(sys.argv) != 3:
-  print "Usage: %s <host> <APIkey>" % sys.argv[0]
+history_id=None
+
+if len(sys.argv) < 3:
+  print "Usage: %s <host> <APIkey> [optional history_id]" % sys.argv[0]
+  sys.exit(1)
+elif len(sys.argv) == 3:
+  pass
+elif len(sys.argv) == 4:
+  history_id=sys.argv[3]
+else:
+  print "Usage: %s <host> <APIkey> [optional history_id]" % sys.argv[0]
   sys.exit(1)
 
-
 gi = galaxy.GalaxyInstance(url=sys.argv[1], key=sys.argv[2])
-
-hl = gi.histories.get_histories()
+hl = gi.histories.get_histories(history_id=history_id)
 
 print json.dumps(hl)
-
